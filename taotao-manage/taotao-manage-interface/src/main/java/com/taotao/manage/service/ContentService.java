5ed19a44e6a2ac40457a5a3a27d9b7a81305194f
lijinming <315768455@qq.com>
2017-11-20 22:46:34 +0800

    11/20

--- taotao-manage/taotao-manage-interface/src/main/java/com/taotao/manage/service/ContentService.java
+++ taotao-manage/taotao-manage-interface/src/main/java/com/taotao/manage/service/ContentService.java
@@ -1,8 +1,7 @@
-package com.taotao.manage.service.imp;
+package com.taotao.manage.service;
 
 import com.taotao.common.vo.DatagridResult;
 import com.taotao.manage.pojo.Content;
-import com.taotao.manage.service.BaseService;
 
 public interface ContentService extends BaseService<Content>{
 	DatagridResult queryListByCCid(Long categoryId,Integer page,Integer rows);
