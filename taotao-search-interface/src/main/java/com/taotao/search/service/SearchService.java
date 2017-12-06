4bd027f37a85c2b3f3ca75a837e70d27167d73ed
Lijinming <315768455@qq.com>
2017-12-06 14:53:23 +0800

    day09 finish

--- taotao-search-interface/src/main/java/com/taotao/search/service/SearchService.java
+++ taotao-search-interface/src/main/java/com/taotao/search/service/SearchService.java
@@ -1,4 +1,10 @@
 package com.taotao.search.service;
 
+import com.taotao.search.pojo.SolrItem;
+
+import java.util.List;
+
 public interface SearchService {
+
+    void saveOrUpdateItemList(List<SolrItem> solrItemList) throws Exception;
 }
--- taotao-search-service/src/main/java/com/taotao/search/service/imp/SearchServiceImp.java
+++ taotao-search-service/src/main/java/com/taotao/search/service/imp/SearchServiceImp.java
@@ -1,9 +1,12 @@
 package com.taotao.search.service.imp;
 
+import com.taotao.search.pojo.SolrItem;
 import com.taotao.search.service.SearchService;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SearchServiceImp implements SearchService {
@@ -11,5 +14,9 @@ public class SearchServiceImp implements SearchService {
     @Autowired
     private HttpSolrServer httpSolrServer;
 
-
+    @Override
+    public void saveOrUpdateItemList(List<SolrItem> solrItemList) throws Exception{
+        httpSolrServer.addBeans(solrItemList);
+        httpSolrServer.commit();
+    }
 }
