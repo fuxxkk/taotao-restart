40bce1081bfe8d0e9f062ba97c82e3773ebf6326
Lijinming <315768455@qq.com>
2017-12-07 17:53:14 +0800

    mq demo init

---
+++ taotao-search-web/src/main/java/com/taotao/search/activemq/ItemMessageListener.java
@@ -0,0 +1,49 @@
+package com.taotao.search.activemq;
+
+import com.taotao.manage.pojo.Item;
+import com.taotao.manage.service.ItemService;
+import com.taotao.search.pojo.SolrItem;
+import com.taotao.search.service.SearchService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.listener.adapter.AbstractAdaptableMessageListener;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.Session;
+
+public class ItemMessageListener extends AbstractAdaptableMessageListener{
+
+    @Autowired
+    private ItemService itemService;
+    @Autowired
+    private SearchService searchService;
+
+    @Override
+    public void onMessage(Message message, Session session)  {
+        try {
+            if (message instanceof MapMessage){
+                MapMessage msg = (MapMessage) message;
+                String type = msg.getString("type");
+                long itemId = msg.getLong("itemId");
+
+                if ("delete".equals(type)){
+                    searchService.deleteItem(itemId);
+                }else {
+                    Item item = itemService.queryById(itemId);
+                    SolrItem solrItem = new SolrItem();
+                    solrItem.setTitle(item.getTitle());
+                    solrItem.setId(item.getId());
+                    solrItem.setImage(item.getImage());
+                    solrItem.setPrice(item.getPrice());
+                    solrItem.setSellPoint(item.getSellPoint());
+                    solrItem.setStatus(item.getStatus());
+                    searchService.saveOrUpdateItem(solrItem);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
