2183c4db93de8a6e08ea6637ca88fb4303c45bb1
Lijinming <315768455@qq.com>
2017-12-08 16:53:13 +0800

    123

--- taotao-item-web/src/main/java/com/taotao/item/activemq/ItemMessageListener.java
+++ taotao-item-web/src/main/java/com/taotao/item/activemq/ItemMessageListener.java
@@ -1,11 +1,14 @@
 package com.taotao.item.activemq;
 
+import org.apache.activemq.command.ActiveMQMapMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.listener.adapter.AbstractAdaptableMessageListener;
 
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.Session;
+import java.util.Map;
 
 public class ItemMessageListener extends AbstractAdaptableMessageListener{
 
@@ -14,6 +17,9 @@ public class ItemMessageListener extends AbstractAdaptableMessageListener{
 
     @Override
     public void onMessage(Message message, Session session) throws JMSException {
+            if(message instanceof MapMessage){
+                ActiveMQMapMessage msg = (ActiveMQMapMessage) message;
 
+            }
     }
 }
